9e511b4db02036435f99da6c1a16e85ba2a84f77
ginger <26461046+gingerchew@users.noreply.github.com>
2023-04-27 14:16:38 -0400

    Adds missing xmlns attribute

--- routes/[dimensions].tsx
+++ routes/[dimensions].tsx
@@ -9,7 +9,7 @@ const Image = ({ x, y }: {x:number; y:number;}) => {
   const textStyles = 'font-family: sans-serif;font-size: clamp(10px, 10cqi, 48px);';
   const svgStyles = 'background-color: #edeff1;container-type: inline-size;'
   const label = (x < 20 || y < 20) ? 'x' : x+'x'+y
-  return `<svg width="${x}" height="${y}" style="${svgStyles}">
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="${x}" height="${y}" style="${svgStyles}">
     <text text-anchor="middle" x="${x / 2}" y="${y / 2}" style="${textStyles}">${label}</text>
   </svg>`
 }
